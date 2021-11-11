493615eb4737894d33c7a9ecaaf5dc8ea656b13d
okbWork <okb3@njit.edu>
2021-11-11 02:07:56 -0500

    altered functions for efficiency

--- calc/calculations/subtraction.py
+++ calc/calculations/subtraction.py
@@ -7,8 +7,4 @@ class Subtraction(Calculation):
     """subtraction calculation object"""
     def get_result(self):
         """get the subtraction results"""
-        difference_of_values = 0.0
-        for value in self.values:
-            difference_of_values =   difference_of_values - value
-            pprint.pprint(value)
-        return difference_of_values
+        return 0 - sum(self.values)
--- tests/subtraction_test.py
+++ tests/subtraction_test.py
@@ -2,7 +2,7 @@
 from calc.calculations.subtraction import Subtraction
 
 def test_calculation_subtraction():
-    """testing that our calculator has a static method for addition"""
+    """testing that our calculator has a static method for subtraction"""
     #Arrange
     mynumbers = (1.0,2.0)
     subtraction = Subtraction(mynumbers)
